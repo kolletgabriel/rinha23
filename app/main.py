6b8e188ad5aeb3a1b955b1299cc913988a1fbd95
Gabriel Kollet <gabrielkollet99@gmail.com>
2025-12-08 21:04:32 -0300

    Stricter error handling and some minor refactoring.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, json
-from psycopg2.errors import UniqueViolation
+from psycopg2.errors import UniqueViolation, InvalidTextRepresentation
 from psycopg2.extensions import TRANSACTION_STATUS_INERROR
 from pydantic import ValidationError
 
@@ -7,36 +7,40 @@ import schema
 import db
 
 
-conn, cur = db.setup_conn()
+CONN, CUR = db.setup_conn()
+
 
 json.provider.DefaultJSONProvider.sort_keys = False
 app = Flask(__name__)
 
 app.register_error_handler(400, lambda _: ('',400))
+app.register_error_handler(InvalidTextRepresentation, lambda _: ('',400))
 app.register_error_handler(404, lambda _: ('',404))
-app.register_error_handler(ValidationError, lambda _: ('',422))
 app.register_error_handler(UniqueViolation, lambda _: ('',422))
+app.register_error_handler(500, lambda _: ('',500))  # shut up
 
-
-@app.teardown_request
-def commit_or_rollback(_):
-    if conn.info.transaction_status is TRANSACTION_STATUS_INERROR:
-        conn.rollback()
-        return
-    conn.commit()
+@app.errorhandler(ValidationError)
+def handle_pydantic_errors(exception):
+    e = exception.errors()
+    for err in e:
+        if err['type'].endswith('_type') or err['type'].startswith('date_'):
+            if err['input'] is not None:
+                return ('', 400)
+            return ('', 422)
+    return ('', 422)
 
 
 @app.route('/users', methods=['POST'])
 def add_user():
     new_user = schema.UserModel(**request.json)
-    cur.execute(db.INSERT_QUERY, new_user.to_tuple())
-    return ('', 201, {'Location': f'/users/{cur.fetchone()['id']}'})
+    CUR.execute(db.INSERT_QUERY, new_user.to_tuple())
+    return ('', 201, {'Location': f'/users/{CUR.fetchone()['id']}'})
 
 
 @app.route('/users/<user_id>')
 def get_user(user_id):
-    cur.execute(db.SELECT_QUERY, (user_id,))
-    result = cur.fetchone()
+    CUR.execute(db.SELECT_QUERY, (user_id,))
+    result = CUR.fetchone()
     if not result:
         return ('', 404)
     return result
@@ -47,11 +51,19 @@ def search_users():
     query = request.args.get('q')
     if not query or query == '\x00':
         return ('', 400)
-    cur.execute(db.SEARCH_QUERY, (f'%{query}%',))
-    return cur.fetchall()
+    CUR.execute(db.SEARCH_QUERY, (f'%{query}%',))
+    return CUR.fetchall()
 
 
 @app.route('/users-count')
 def count_users():
-    cur.execute(db.COUNT_QUERY)
-    return cur.fetchone()['result']
+    CUR.execute(db.COUNT_QUERY)
+    return CUR.fetchone()['result']
+
+
+@app.teardown_request
+def commit_or_rollback(_):
+    if CONN.info.transaction_status is TRANSACTION_STATUS_INERROR:
+        CONN.rollback()
+        return
+    CONN.commit()
